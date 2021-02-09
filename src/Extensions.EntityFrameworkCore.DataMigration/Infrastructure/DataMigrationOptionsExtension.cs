59b890892a718791094ffe50d33ad2535d0c08b4
Raphael Schwarz <raphael@codeworx.at>
2021-02-09 16:43:49 +0100

    Add EF Core 3.1 and 5.0

--- src/Extensions.EntityFrameworkCore.DataMigration/Infrastructure/DataMigrationOptionsExtension.cs
+++ src/Extensions.EntityFrameworkCore.DataMigration/Infrastructure/DataMigrationOptionsExtension.cs
@@ -11,11 +11,22 @@ namespace Extensions.EntityFrameworkCore.DataMigration.Infrastructure
         public DataMigrationOptionsExtension(DataMigrationOptions migrationOptions)
         {
             _migrationOptions = migrationOptions;
+#if !EF2_2
+            Info = new DataMigrationOptionsExtensionInfo(this);
+#endif
         }
 
+#if EF2_2
         public string LogFragment => "SqlServerBulk";
+#else
+        public DbContextOptionsExtensionInfo Info { get; }
+#endif
 
+#if EF2_2
         public bool ApplyServices(IServiceCollection services)
+#else
+        public void ApplyServices(IServiceCollection services)
+#endif
         {
             services.AddSingleton(_migrationOptions);
             var oldCustomizer = services.First(p => p.ServiceType == typeof(IModelCustomizer));
@@ -24,16 +35,48 @@ namespace Extensions.EntityFrameworkCore.DataMigration.Infrastructure
             var newCustomizer = typeof(DataMigrationModelCustomizer<>).MakeGenericType(oldCustomizer.ImplementationType);
             services.Add(ServiceDescriptor.Describe(typeof(IModelCustomizer), newCustomizer, oldCustomizer.Lifetime));
 
+#if EF2_2
             return true;
-        }
-
-        public virtual long GetServiceProviderHashCode()
-        {
-            return _migrationOptions.GetHashCode() * 9909;
+#endif
         }
 
         public virtual void Validate(IDbContextOptions options)
         {
         }
+
+#if EF2_2
+        public virtual long GetServiceProviderHashCode()
+        {
+            return _migrationOptions.GetHashCode() * 9909;
+        }
+#else
+        private class DataMigrationOptionsExtensionInfo : DbContextOptionsExtensionInfo
+        {
+            private readonly DataMigrationOptionsExtension _extension;
+
+            public DataMigrationOptionsExtensionInfo(DataMigrationOptionsExtension extension)
+                : base(extension)
+            {
+                _extension = extension;
+            }
+
+            public override bool IsDatabaseProvider => false;
+
+            public override string LogFragment => "SqlServerBulk";
+
+            public override long GetServiceProviderHashCode()
+            {
+                return _extension._migrationOptions.GetHashCode() * 9909;
+            }
+
+            public override void PopulateDebugInfo(System.Collections.Generic.IDictionary<string, string> debugInfo)
+            {
+                if (!string.IsNullOrWhiteSpace(_extension._migrationOptions.MigrationAssembly))
+                {
+                    debugInfo.Add("DataMigrations.MigrationAssembly", _extension._migrationOptions.MigrationAssembly);
+                }
+            }
+        }
+#endif
     }
 }
--- tools/build.ps1
+++ tools/build.ps1
@@ -7,6 +7,27 @@ New-NugetPackages `
     -Projects $projects `
     -NugetServerUrl "http://www.nuget.org/api/v2" `
     -VersionPackage "Extensions.EntityFrameworkCore.DataMigration" `
-    -VersionFilePath "..\version.json" `
+    -VersionFilePath "..\version-22.json" `
     -OutputPath "..\dist\nuget\" `
-    -MsBuildParams "SourceLinkCreate=true;SignAssembly=true;AssemblyOriginatorKeyFile=..\..\private\signkey-ef-datamigrations.snk"
+    -MsBuildParams "SignAssembly=true;AssemblyOriginatorKeyFile=..\..\private\signkey-ef-datamigrations.snk;Configuration=EF22"
+
+New-NugetPackages `
+    -Projects $projects `
+    -DoNotCleanOutput `
+    -NugetServerUrl "http://www.nuget.org/api/v2" `
+    -VersionPackage "Extensions.EntityFrameworkCore.DataMigration" `
+    -VersionFilePath "..\version-31.json" `
+    -OutputPath "..\dist\nuget\" `
+    -MsBuildParams "SignAssembly=true;AssemblyOriginatorKeyFile=..\..\private\signkey-ef-datamigrations.snk;Configuration=EF31"
+
+$coreVersion = New-NugetPackages `
+    -Projects $projects `
+    -DoNotCleanOutput `
+    -NugetServerUrl "http://www.nuget.org/api/v2" `
+    -VersionPackage "Extensions.EntityFrameworkCore.DataMigration" `
+    -VersionFilePath "..\version-50.json" `
+    -OutputPath "..\dist\nuget\" `
+    -MsBuildParams "SignAssembly=true;AssemblyOriginatorKeyFile=..\..\private\signkey-ef-datamigrations.snk;EfVersion=50"
+
+
+Write-Host "##vso[build.updatebuildnumber]$($coreVersion.NugetVersion)"
